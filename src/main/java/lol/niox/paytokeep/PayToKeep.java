e667ad8288cd6aa2edfb30f0792aa390179489da
Zhixiang Feng <2616718f@student.gla.ac.uk>
2024-04-10 23:53:03 +0100

    implement buyinv command

--- src/main/java/lol/niox/paytokeep/PayToKeep.java
+++ src/main/java/lol/niox/paytokeep/PayToKeep.java
@@ -1,5 +1,7 @@
 package lol.niox.paytokeep;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import net.milkbowl.vault.chat.Chat;
 import net.milkbowl.vault.economy.Economy;
 import net.milkbowl.vault.permission.Permission;
@@ -9,10 +11,21 @@ import org.bukkit.entity.Player;
 import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public final class PayToKeep extends JavaPlugin {
     private static Economy econ = null;
     private static Permission perms = null;
     private static Chat chat = null;
+    private static Map<String, List<Boolean>> data;
+    private static int price;
 
     @Override
     public void onEnable() {
@@ -25,25 +38,70 @@ public final class PayToKeep extends JavaPlugin {
         }
         setupPermissions();
         setupChat();
-    }
 
-    @Override
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        Player player = (Player) sender;
-        // /buyinv
-        if (command.getName().equalsIgnoreCase("buyinv")) {}
+        Path filePath = Paths.get("./PayToKeepData/data.json");
 
-        // /setinvprice
-        if (command.getName().equalsIgnoreCase("setinvprice")) {}
+        if (!Files.exists(filePath)) {
+            try {
+                Files.createDirectories(filePath.getParent());
+                Files.createFile(filePath);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        try {
+            loadJsonData(filePath.toString());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
-        // /salvage
-        if (command.getName().equalsIgnoreCase("salvage")) {}
-        return false;
     }
 
     @Override
     public void onDisable() {
         getLogger().info(String.format("[%s] Disabled Version %s", getDescription().getName(), getDescription().getVersion()));
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
+        // /buyinv
+        if (command.getName().equalsIgnoreCase("buyinv")) {
+            if (sender instanceof Player) {
+                Player player = (Player) sender;
+                String playerUUID = player.getUniqueId().toString();
+                if (data.containsKey(playerUUID)) {
+                    if (data.get(playerUUID).get(0)) {
+                        player.sendMessage("你不许再买了！");
+                        return true;
+                    }
+                } else {
+                    List<Boolean> booleanList = Arrays.asList(false, true);
+                    data.put(playerUUID, booleanList);
+                }
+                if (econ.getBalance(player) < price) {
+                    player.sendMessage("穷鬼，钱够了再来！");
+                    return true;
+                } else {
+                    econ.withdrawPlayer(player, price);
+                    data.get(playerUUID).set(0, true);
+                    player.sendMessage("你现在可以随便死一次了！");
+                    saveJsonData("./PayToKeepData/data.json", data, price);
+                    return true;
+                }
+
+            }
+        }
+
+        // /setinvprice
+        if (command.getName().equalsIgnoreCase("setinvprice")) {}
+
+        // /switchkeep
+        if (command.getName().equalsIgnoreCase("switchkeep")) {}
+
+        // /salvage
+        if (command.getName().equalsIgnoreCase("salvage")) {}
+        return false;
     }
 
     private boolean setupEconomy() {
@@ -69,4 +127,36 @@ public final class PayToKeep extends JavaPlugin {
         perms = rsp.getProvider();
         return perms != null;
     }
+
+    private void loadJsonData(String filePath) throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        Path path = Paths.get(filePath);
+        Map<String, Object> loadedJson = ((Map<String, Object>) objectMapper.readValue(path.toFile(), Map.class));
+
+        // Check if file is empty, if so initialize data with an empty map
+        if (Files.size(path) == 0) {
+            data = new HashMap<>();
+            price = 1000;
+        } else {
+            // Read the file and convert its content to a Map<String, boolean[]>
+            data = loadedJson.get("data") == null ? new HashMap<>() : (Map<String, List<Boolean>>) loadedJson.get("data");
+            price = (int) loadedJson.get("price");
+        }
+    }
+
+    private void saveJsonData(String filePath, Map<String, List<Boolean>> data, int price) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        Path path = Paths.get(filePath);
+        Map<String, Object> dataToWrite = new HashMap<>();
+        dataToWrite.put("data", data);
+        dataToWrite.put("price", price);
+
+        // Write the data to the file
+        try {
+            objectMapper.writeValue(path.toFile(), dataToWrite);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }

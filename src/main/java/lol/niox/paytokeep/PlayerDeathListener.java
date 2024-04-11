132d33f128cc4dcce8b295460245483d2fc8bbbc
Zhixiang Feng <2616718f@student.gla.ac.uk>
2024-04-11 02:49:57 +0100

    reset status after recovering

--- src/main/java/lol/niox/paytokeep/PlayerDeathListener.java
+++ src/main/java/lol/niox/paytokeep/PlayerDeathListener.java
@@ -3,6 +3,7 @@ package lol.niox.paytokeep;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.player.PlayerRespawnEvent;
 
 import java.util.List;
 import java.util.Map;
@@ -20,6 +21,18 @@ public class PlayerDeathListener implements Listener {
                 return;
             }
         }
+    }
 
+    @EventHandler
+    public void onPlayerRespawn(PlayerRespawnEvent event) {
+        Map<String, List<Boolean>> data = PayToKeep.getData();
+        String playerID = event.getPlayer().getUniqueId().toString();
+        if (data.containsKey(playerID)) {
+            if (data.get(playerID).get(0) && data.get(playerID).get(1)) {
+                data.get(playerID).set(0, false);
+                event.getPlayer().sendMessage("库存已恢复");
+                return;
+            }
+        }
     }
 }
